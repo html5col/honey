6811f1fb22551b8556ada6cc1958d5f413aaf409
html5col <html5master@sina.com>
2016-07-20 11:07:44 +0800

    #partial files and custom js and css

--- app.js
+++ app.js
@@ -5,10 +5,37 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+/****hbs section*****/
 var hbs = require('hbs');
+//For convenience, registerPartials provides a quick way to load all partials from a specific directory:
+hbs.registerPartials(__dirname + '/views/partials');
+
 hbs.registerHelper('static',function(name){
   return require('./library/static.js').map(name);
 });
+var blocks = {};
+
+//the below extend and block from hbs official site example:https://github.com/donpark/hbs/blob/master/examples/extend/app.js
+//so we can have custom scripts or stylesheets on your pages
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+  if (!block) {
+    block = blocks[name] = [];
+  }
+
+  block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+
+  // clear the block
+  blocks[name] = [];
+  return val;
+});
+
+/****hbs section****/
 
 
 
@@ -21,6 +48,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -36,10 +65,17 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
+//--pass data to partials
+// app.use(function(req,res,next){
+//   if(!res.locals.partials) res.locals.partials = {};
+//   res.locals.partials.weather = {
+//     quotes: [
+//       {data1: 'Only those who want to change the world aare the ones who do!' },
+//       {data2: 'Just do it!'}
+//     ]
+//   };
+//   next();
+// });
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // console.log(req.headers.host);
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
