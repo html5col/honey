54f8558c1db92b40b5abb342d000cdd6afd0532e
html5col <html5master@sina.com>
2016-07-21 16:14:11 +0800

    add the wecht hover image

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // console.log(req.headers.host);
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Seven Bears 野生蜂蜜' });
 
 });
 
